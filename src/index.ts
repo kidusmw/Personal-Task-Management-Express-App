cbc1bf9aa139504aa92911d46a4562c11da78b31
Kidus <kiduswubie7110@gmail.com>
2026-05-08 10:16:19 +0300

    wire auth routes to express app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import authRouter from "./routes/auth";
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
+
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
