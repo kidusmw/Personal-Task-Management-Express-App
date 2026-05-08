73dbc3a397ca8f61aae25cec50a62c5b51f5e020
Kidus <kiduswubie7110@gmail.com>
2026-05-08 10:01:04 +0300

    feat: add auth controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { register, login } from "../services/auth";
+
+export async function registerUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const result = await register(firstName, lastName, email, password);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function loginUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { email, password } = req.body;
+    const result = await login(email, password);
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+}
