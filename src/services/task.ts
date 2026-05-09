aaab9790a43a6085e8d43bdad7d270f67774c961
Kidus <kiduswubie7110@gmail.com>
2026-05-09 15:45:06 +0300

    feat: add task service functions

--- src/services/task.ts
+++ src/services/task.ts
@@ -1,10 +1,11 @@
 import prisma from "../lib/prisma";
+import { Status } from "../generated/prisma/enums";
 
-export async function GetTasks(
+export async function getTasks(
   userId: number,
   page: number,
   limit: number,
-  search: string,
+  search?: string,
 ) {
   const skip = (page - 1) * limit;
   const take = limit;
@@ -29,4 +30,56 @@ export async function GetTasks(
       totalPages: Math.ceil(total / limit),
     },
   };
+}
+
+export async function getTasksById(userId: number, taskId: number) {
+  const task = await prisma.task.findFirst({
+    where: { userId, id: taskId },
+  });
+
+  if (!task) {
+    throw new Error("Task Not Found");
+  }
+  return task;
+}
+
+export async function createTask(
+  userId: number,
+  subject: string,
+  description: string,
+  status: Status,
+  deadline?: Date,
+) {
+  const new_task = await prisma.task.create({
+    data: {
+      subject,
+      description,
+      status,
+      deadline,
+      user: { connect: { id: userId } },
+    },
+  });
+  return new_task;
+}
+
+export async function updateTask(
+  taskId: number,
+  userId: number,
+  data: Partial<{
+    subject: string;
+    description: string;
+    status: Status;
+    deadline: Date;
+  }>,
+) {
+  return prisma.task.update({
+    where: { id: taskId, userId },
+    data,
+  });
+}
+
+export async function deleteTask(userId: number, taskId: number) {
+  return prisma.task.delete({
+    where: { id: taskId, userId },
+  });
 }
