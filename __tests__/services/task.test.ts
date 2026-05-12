46183eddab1d5e8fc8135468c6a434fb8466332c
Kidus <kiduswubie7110@gmail.com>
2026-05-13 01:55:45 +0300

    test: add unit test for createTask

--- __tests__/services/task.test.ts
+++ __tests__/services/task.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, expect } from "vitest";
 import { prismaMock } from "../helpers/prismaMock";
-import { getTasksById } from "../../src/services/task";
+import { getTasksById, createTask } from "../../src/services/task";
 import { Status } from "../../src/generated/prisma/enums";
+import { fa } from "zod/v4/locales";
 
 describe("getTaskById", () => {
   // HAPPY PATH
@@ -24,5 +25,26 @@ describe("getTaskById", () => {
   it('should throw "Task Not Found" when task does not exist', async () => {
     prismaMock.task.findFirst.mockResolvedValue(null);
     await expect(getTasksById(1, 1)).rejects.toThrow("Task Not Found");
+  });
+});
+
+describe("createTask", () => {
+  it("should return a new task when valid data is provided", async () => {
+    const fakeTask = {
+      id: 1,
+      subject: "Test Subject",
+      description: "Test Description",
+      status: "PENDING" as Status,
+      deadline: null,
+      userId: 1,
+      createdAt: new Date(),
+    };
+    prismaMock.task.create.mockResolvedValue(fakeTask);
+    const result = await createTask(1, {
+      subject: fakeTask.subject,
+      description: fakeTask.description,
+      status: fakeTask.status,
+    });
+    expect(result).toEqual(fakeTask);
   });
 });
