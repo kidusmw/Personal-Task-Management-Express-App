0e2fb837bc8ef9424b3e9b6ccedbed32d78315d1
Kidus <kiduswubie7110@gmail.com>
2026-05-16 18:31:01 +0300

    test: add unit test for login

--- __tests__/services/auth.test.ts
+++ __tests__/services/auth.test.ts
@@ -1,35 +1,51 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { prismaMock } from "../helpers/prismaMock";
 import { register, login } from "../../src/services/auth";
-import { rejects } from "assert/strict";
+
+vi.mock("bcryptjs", () => ({
+  default: {
+    hash: vi.fn().mockResolvedValue("hashedpassword"),
+    compare: vi.fn().mockResolvedValue(true),
+  },
+}));
+
+const fakeUser = {
+  id: 1,
+  firstName: "test",
+  lastName: "test",
+  email: "test@example.com",
+  password: "hashedpassword",
+  createdAt: new Date(),
+};
 
 describe("register", () => {
-  const fakeUser = {
-    id: 1,
-    firstName: "test",
-    lastName: "test",
-    email: "test@example.com",
-    password: "secret123",
-    createdAt: new Date(),
-    tasks: [],
-  };
+  // HAPPY PATH
   it("should return a new user when firstName, lastName, email and password are provided", async () => {
     prismaMock.user.create.mockResolvedValue(fakeUser);
     const result = await register(
       fakeUser.firstName,
       fakeUser.lastName,
       fakeUser.email,
-      fakeUser.password,
+      "secret123",
     );
     expect(result).toEqual(fakeUser);
   });
 
   // SAD PATH
   it("should throw an error when user already registered", async () => {
-    const { firstName, lastName, email, password } = fakeUser;
+    const { firstName, lastName, email } = fakeUser;
     prismaMock.user.findUnique.mockResolvedValue(fakeUser);
     await expect(
-      register(firstName, lastName, email, password),
+      register(firstName, lastName, email, "secret123"),
     ).rejects.toThrow("Account already exists");
+  });
+});
+
+describe("login", () => {
+  it("should return a logged In user when valid email and password provided", async () => {
+    const { email } = fakeUser;
+    prismaMock.user.findUnique.mockResolvedValue(fakeUser);
+    const result = await login(email, "secret123");
+    expect(result).toHaveProperty("token");
   });
 });
